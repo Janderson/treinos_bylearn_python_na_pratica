6782ec09dd4594a1838e3da594d5cc6ede8dcaa0
Janderson <ffjanderson@gmail.com>
2022-05-11 21:13:39 -0300

    update do micro-projeto profsms

--- modulo6_lacos/profsms.py
+++ modulo6_lacos/profsms.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 DEBUG = False
 
 QUERY_PROFESSORES = ["Jose", "Roberto", "Rose"]
@@ -23,42 +25,71 @@ def main():
     autor = "Janderson"
     print(f"\n*** Prof SMS v1.0 *** (por {autor})\n")
 
+    mensagem_template = (
+        "Ola {aluno}, seu professor {professor} " 
+        "não comparecerá a aula hoje"
+    )
 
-    mensagem_template = "Ola {aluno}, seu professor {professor} não comparecerá a aula hoje"
+    # + Criar uma variável 'repetir_loop' para controle de loops
+    # + Criar um loop que executa x vezes
+    repetir_loop = 5
+    for loopid in range(repetir_loop):
 
-    repetir_loop = 2
-    for i in range(repetir_loop):
+        # Buscar todos os professores da querie de professores
+        # Se estiver no modo de Depuração mostrar nome do professor
         for professor in QUERY_PROFESSORES:
-            if DEBUG: print(f"\n| -> Professor: {professor}")
+            if DEBUG:
+                print(f"\n| -> Professor: {professor}")
 
-            devo_avisar_alunos = all([esta_ausente 
+            # Criar lógica condicional saber se alunos devem ser avisados
+            # Variavel devo_avisar_alunos
+            devo_avisar_alunos = all([
+                    esta_ausente
                                       for prof, esta_ausente in QUERY_PROFESSORES_AUSENTES 
-                                      if prof == professor])
-            alunos_do_prof_ja_avisados = (professor in TABELA_ALUNOS_AVISADOS)
+                    if prof == professor
+                ])
 
-            if DEBUG: print(f"| -> devo_avisar_alunos: {devo_avisar_alunos} "
-                            f"alunos_do_prof_ja_avisados: {alunos_do_prof_ja_avisados}")
+            # Criar logica condicional para saber se alunos ja foram avisados anteriormente
+            # Variavel devo_avisar_alunos
+            alunos_do_prof_ja_avisados = professor in TABELA_ALUNOS_AVISADOS
 
+            if DEBUG:
+                print(
+                    f"| -> devo_avisar_alunos: {devo_avisar_alunos} "
+                    f"alunos_do_prof_ja_avisados: {alunos_do_prof_ja_avisados}"
+                )
+
+            # Criar logica condicional para saber se alunos ja foram avisados anteriormente
+            # Variavel devo_avisar_alunos
             if devo_avisar_alunos and not alunos_do_prof_ja_avisados:
-                if DEBUG: print(f"| --> Professor {professor} ==> irá faltar, avisar alunos")
+                if DEBUG:
+                    print(f"| --> Professor {professor} ==> irá faltar, avisar alunos")
+
+                # Adicionar professor a tabela de alunos avisados
                 TABELA_ALUNOS_AVISADOS.append(professor)
             
+                # Criar loop para pegar alunos do professor
                 for query_item in QUERY_ALUNOS_PROFESSORES:
+
+                    # extrair professor_aluno e alunos na mesma linha
                     professor_aluno, alunos = query_item[0], query_item[1]
-                    if DEBUG: print(f"| ---> ProfessorAluno: {professor_aluno} alunos: {alunos}")
-                    if professor_aluno == professor:
+
+                    if DEBUG:
+                        print(
+                            f"| ---> ProfessorAluno: {professor_aluno} alunos: {alunos}"
+                        )
+
+                    if professor_aluno != professor:
+                        continue
+
+                    # Print MSG gerado apartir da template na parte superior
                         for aluno in alunos:
-                            msg_text = mensagem_template.format(aluno=aluno, professor=professor)
+                        msg_text = mensagem_template.format(
+                            aluno=aluno, professor=professor
+                        )
                             print(f"Enviando SMS para {aluno}, msg ===> ({msg_text})")
+        print(f"fim da interacao ({loopid}), aguardando...")
+        sleep(2)
 
 
 main()
-
-
-def historia():
-    # Repetir um loop simples algumas vezes, comando range
-    for numero in range(10):
-        print(f"Loop {numero}")
-
-
-historia()
