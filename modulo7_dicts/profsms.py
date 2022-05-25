0ecc6445621599853b8fa0c7838a3e40c1260309
Janderson <ffjanderson@gmail.com>
2022-05-25 20:55:46 -0300

    profsms: update comentários

--- modulo7_dicts/profsms.py
+++ modulo7_dicts/profsms.py
@@ -38,9 +38,10 @@ def main():
     for loopid in range(repetir_loop):
 
         # Buscar todos os professores da querie de professores
-        # Se estiver no modo de Depuração mostrar nome do professor
         for professor_item in QUERY_PROFESSORES:
             professor = professor_item.get("professor")
+
+            # Se estiver no modo de depuração mostrar nome do professor
             if DEBUG:
                 print(f"\n| -> Professor: {professor} {professor_item.get('ausente')}")
 
@@ -52,15 +53,17 @@ def main():
             # Variavel devo_avisar_alunos
             alunos_do_prof_ja_avisados = professor_item.get("avisado")
 
+            # Criar condição para saber se deve enviar sms para professor
+            # + lógica deve saber se alunos já foram avisados anteriormente
             condicao_para_envio = devo_avisar_alunos and not alunos_do_prof_ja_avisados
+
+            # Mostrar debug da condicao_para_envio
             if DEBUG:
                 print(
                     f"| -> CONDIÇÃO({condicao_para_envio}): devo_avisar_alunos: {devo_avisar_alunos} "
                     f"alunos_do_prof_ja_avisados: {alunos_do_prof_ja_avisados}"
                 )
 
-            # Criar logica condicional para saber se alunos ja foram avisados anteriormente
-            # Variavel devo_avisar_alunos
             if condicao_para_envio:
                 # Adicionar professor a tabela de alunos avisados
                 professor_item["avisado"] = True
@@ -72,11 +75,15 @@ def main():
                         print(
                             f"| ---> Avisando professorAluno: {professor} alunos: {aluno}"
                         )
-
+                    # montar template da msg
                     msg_text = mensagem_template.format(
                         aluno=aluno, professor=professor
                     )
+                    
+                    # Executar envio -> mostrar print
                     print(f"Enviando SMS para {aluno}, msg ===> ({msg_text})")
+
+        # print de fim da iteração + sleep
         print(f"fim da interacao ({loopid}), aguardando...")
         sleep(2)
 
